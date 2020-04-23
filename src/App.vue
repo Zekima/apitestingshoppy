00cd3b437b43de5a346e977ba02d266159144422
Your Name <you@example.com>
2020-04-23 11:34:31 +0200

    md

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@ export default {
   mounted: function () {
     this.$http
       .get('https://shoppy.gg/api/v1/products/') 
-      .set({ 'Authorization': 'miRtovzMxnHyGomQGTewS7WIxzaS3csQaTGKSh9vNRIJurFxSO', 'Access-Control-Allow-Origin': 'https://shoppy.gg', 'Access-Control-Allow-Methods':'GET, POST'})
+      .set({ 'Authorization': 'miRtovzMxnHyGomQGTewS7WIxzaS3csQaTGKSh9vNRIJurFxSO', 'Access-Control-Allow-Origin': 'https://localhost:8080', 'Access-Control-Allow-Methods':'GET, POST'})
       .set('Accept', 'application/json')
       .then(res => console.log(res))
       .catch(err => console.log(err));
